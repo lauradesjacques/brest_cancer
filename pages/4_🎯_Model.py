338ec496bdc45e09f74590682898a3404c99ffb8
lauradesjacques <desjacques.lauralee@gmail.com>
2023-05-12 16:20:47 +0200

    return old version

--- pages/4_🎯_Model.py
+++ pages/4_🎯_Model.py
@@ -1,15 +1,21 @@
 import streamlit as st
 from predict import prediction_cancer
 from services import create_table, save_formulaire
+import streamlit.components.v1 as components
 import pymysql
 
 st.set_page_config(layout="wide")
+
 
 def create_form():
     st.title('Patient data form')
 
     # Connexion à la BDD.
-    conn=pymysql.connect(host='localhost',port=int(3306),user='root', passwd='Root1234+', db='cancer')
+    conn = pymysql.connect(host='localhost',
+                           port=int(3306),
+                           user='root',
+                           passwd='Root1234+',
+                           db='cancer')
 
     # On créer la table de prédiction s'il n'existe pas.
     create_table(table_name="resultat")
@@ -34,14 +40,14 @@ def create_form():
         "f4": f4
     }
 
-# Si le medecin clique sur le boutton
+    # Si le medecin clique sur le boutton
     if submit_button:
 
-    # Utilisation du modèle de prédiction.
-    # On appelle la fonction prediction_cancer() et on lui passe les réponses de l'utilisateur.
+        # Utilisation du modèle de prédiction.
+        # On appelle la fonction prediction_cancer() et on lui passe les réponses de l'utilisateur.
         pred = prediction_cancer(features=[i for i in formulaire.values()])
 
-    # On inscrit la prédiction dans le dictionnaire.
+        # On inscrit la prédiction dans le dictionnaire.
         if pred:
             formulaire["predict"] = "Cancer"
         else:
@@ -53,9 +59,31 @@ def create_form():
 
         # Affichage de la prédictions.
         if pred:
-            st.header("Cancer !")
+            components.html("""
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+     <div class="modal-content rounded-3 shadow bg-danger">
+      <div class="modal-body p-4 text-center">
+        <h5 class="mb-0">Diagnosis : Brest Cancer</h5>
+        <p class="mb-0">Malignant Tumor</p>
+      </div>
+    </div>
+    """)
         else:
-            st.header("Pas de cancer")
+            components.html("""
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+   <div class="modal-content rounded-3 shadow bg-success">
+      <div class="modal-body p-4 text-center">
+        <h5 class="mb-0">Diagnosis : No Cancer</h5>
+        <p class="mb-0">Benign Tumor</p>
+      </div>
+    </div>
+    """)
 
 
 create_form()
